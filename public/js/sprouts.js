22ed8c444305a90aaf6d78bcbf12035a65352cb7
Nick Blaisdell <nick.blaisdell@gmail.com>
2016-12-05 12:32:58 -0500

    works without optional

--- public/js/sprouts.js
+++ public/js/sprouts.js
@@ -1,1 +1,15 @@
-// YOUR CODE GOES HERE
+var page = 2;
+
+$('.more-sprouts').on('click', function(event) {
+  event.preventDefault();
+
+  var request = $.ajax({
+    method: "GET",
+    url: "/tweets.json?page=" + page,
+  }).done(function(data) {
+    data.forEach(function(tweet) {
+      $(".tweets").append("<li class='tweet'> <div class='body'>" + tweet.text + "</div> <div class='user'>" + tweet.username + "</div> </li>");
+    })
+    page++;
+  });
+});
